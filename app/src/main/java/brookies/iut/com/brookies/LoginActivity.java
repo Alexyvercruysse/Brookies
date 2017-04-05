563ed1addcde8aa9fb3d85096dbc0a5da5ed08de
mgaucher <marti.gaucher@hotmail.fr>
2017-04-05 15:43:45 +0200

    Adding profile page

--- app/src/main/java/brookies/iut/com/brookies/LoginActivity.java
+++ app/src/main/java/brookies/iut/com/brookies/LoginActivity.java
@@ -43,7 +43,8 @@ public class LoginActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        Intent intent = new Intent(LoginActivity.this,ProfileActivity.class);
+            startActivity(intent);
         mAuth = FirebaseAuth.getInstance(); // Connexion FireBase
 
 
